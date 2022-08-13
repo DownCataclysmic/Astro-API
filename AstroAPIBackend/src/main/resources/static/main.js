61abf9d98eb9c9ce4599f4523a24c0961dd8a1db
Danny Maksoud <dannymaksoud@gmail.com>
2022-08-13 01:48:30 +0100

    Managed to separate JSON date so I can manipulate them to use in different parts of HTML

--- AstroAPIBackend/src/main/resources/static/main.js
+++ AstroAPIBackend/src/main/resources/static/main.js
@@ -47,26 +47,35 @@ async function getStarDataO() {
   const rand_star = data[getRandomInt(1,5)]
   /*Now I know what you're thinking. What on earth is this mess below this comment.
     How about we just be happy that it works :)*/
-  const myJSON = JSON.stringify(rand_star);
-  jsonStarName = myJSON.replace("\"starClass\":", "\"Star Classification\":");
-  jsonStarName = jsonStarName.replace("\"id\":", "\"Star ID\":");
-  jsonStarName = jsonStarName.replace("\"starName\":", "\"Star Name\":");
-  jsonStarName = jsonStarName.replace("\"starMass\":", "\"Star Mass (M☉)\":");
-  jsonStarName = jsonStarName.replace("\"starRadius\":", "\"Star Radius (R☉)\":");
-  jsonStarName = jsonStarName.replace("\"starColour\":", "\"Star Colour\":");
-  jsonStarName = jsonStarName.replace("\"starTemp\":", "\"Star Temperature (K)\":");
-  jsonStarName = jsonStarName.replace("\"starDistFromEarth\":", "\"Star Distance From Earth (Parsecs)\":");
-  jsonStarName = jsonStarName.replace("\"starBrightness\":", "\"Star Brightness (Apparent Magnitude)\":");
 
-  let rand_star_fixed = JSON.parse(jsonStarName);
-  const generatedHtml = Object.keys(rand_star_fixed).reduce((accum, currKey) => accum +
+  let myJSONcol1 = JSON.stringify(rand_star, ["starClass", "id", "starName", "starName", "starRadius", "starColour", "starTemp","starDistFromEarth" ,"starBrightness" ]);
+  jsonStarNameCol1 = myJSONcol1.replace("\"starClass\":", "\"Star Classification\":"); jsonStarNameCol1 = jsonStarNameCol1.replace("\"id\":", "\"Star ID\":");
+  jsonStarNameCol1 = jsonStarNameCol1.replace("\"starName\":", "\"Star Name\":");jjsonStarNameCol1 = jsonStarNameCol1.replace("\"starMass\":", "\"Star Mass (M☉)\":");
+  jsonStarNameCol1 = jsonStarNameCol1.replace("\"starRadius\":", "\"Star Radius (R☉)\":");jsonStarNameCol1 = jsonStarNameCol1.replace("\"starColour\":", "\"Star Colour\":");
+  jsonStarNameCol1 = jsonStarNameCol1.replace("\"starTemp\":", "\"Star Temperature (K)\":");jsonStarNameCol1 = jsonStarNameCol1.replace("\"starDistFromEarth\":", "\"Star Distance From Earth (Parsecs)\":");  
+  jsonStarNameCol1 = jsonStarNameCol1.replace("\"starBrightness\":", "\"Star Brightness (Apparent Magnitude)\":");
+  let rand_star_col1 = JSON.parse(jsonStarNameCol1);
+  
+  let myJSONcol2 = JSON.stringify(rand_star, ["starImgURL", "starDescr"]);      
+  jsonStarNameCol2 = myJSONcol2.replace("\"starDescr\"", "\"\"");
+  jsonStarNameCol2 =  jsonStarNameCol2.replace("\"starImgURL\"", "\"\"");
+  let rand_star_col2 = JSON.parse(jsonStarNameCol2);
+
+  const generatedHtmlCol1 = Object.keys(rand_star_col1).reduce((accum, currKey) => accum +
   `<div class="p-2">
-    <h3 class="o-stars">${currKey}: ${rand_star_fixed[currKey]}</h3>
+    <h3 class="o-stars">${currKey}: ${rand_star_col1[currKey]}</h3>
     </div>`, '');
 
-          document.getElementById('container-oa').innerHTML = generatedHtml;
-          document.getElementById('container-ob').innerHTML = "This column is ready for the description and image";
-        }
+          document.getElementById('container-oa').innerHTML = generatedHtmlCol1;
+
+  const generatedHtmlCol2 = Object.keys(rand_star_col2).reduce((accum, currKey) => accum +
+    `<div class="p-2">
+      <h3 class="o-stars">${currKey} ${rand_star_col2[currKey]}</h3>
+      </div>`, '');
+
+          document.getElementById('container-ob').innerHTML = generatedHtmlCol2;
+
+}
 
 async function getStarDataB() {
   const response = await fetch(star_url);
