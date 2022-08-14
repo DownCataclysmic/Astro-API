65d818152d244bc7d89e3932746e281556375108
Danny Maksoud <dannymaksoud@gmail.com>
2022-08-14 16:04:57 +0100

    Really super annoying error fixed

--- AstroAPIBackend/src/main/java/com/qa/astroAPIBackend/repo/AstroRepo.java
+++ AstroAPIBackend/src/main/java/com/qa/astroAPIBackend/repo/AstroRepo.java
@@ -2,6 +2,7 @@ package com.qa.astroAPIBackend.repo;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
