03df5f7b50bfe140ab556d143ea6339ce1629b75
Danny Maksoud <dannymaksoud@gmail.com>
2022-08-14 01:34:43 +0100

    Started trying to get the gallery to work. So far I have only managed to get one image to show up

--- AstroAPIBackend/src/main/java/com/qa/astroAPIBackend/repo/AstroRepo.java
+++ AstroAPIBackend/src/main/java/com/qa/astroAPIBackend/repo/AstroRepo.java
@@ -2,10 +2,8 @@ package com.qa.astroAPIBackend.repo;
 
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.qa.astroAPIBackend.models.Stars;
 
