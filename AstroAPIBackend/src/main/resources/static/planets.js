89cf44f38c1f924866d185adc63e2435ae5f5be6
Danny Maksoud <dannymaksoud@Dannys-MacBook-Air.local>
2022-08-15 21:42:44 +0100

    This was a disaster. Trying to work from a PC to a Macbook just before the deadline was probably one of the worst decisions I could have made. This has set me back loads and for some reason a lot of things are broken and have had to be reverted. Unlucky I guess.

--- AstroAPIBackend/src/main/resources/static/planets.js
+++ AstroAPIBackend/src/main/resources/static/planets.js
@@ -51,126 +51,129 @@ async function getPlanetDataTerrestrial() {
               document.getElementById('container-planet-t-b').innerHTML = generatedHtmlCol3 + generatedHtmlCol2;
   
   }
-async function getGalaxyDataElliptical() {
-    const response = await fetch(galaxy_url);
-    const data = await response.json();
-    const rand_galaxy = data[getRandomInt(4,8)]
-    /*Now I know what you're thinking. What on earth is this mess below this comment.
-      How about we just be happy that it works :)*/
-  
-      let myJSONcol1 = JSON.stringify(rand_galaxy, ["galaxyName", "id", "galaxyNumStars", "galaxyDist", "galaxyWidth", "galaxyBrightness"]);
-      jsonGalaxyNameCol1 = myJSONcol1.replace("\"galaxyName\":", "\"Galaxy Name\":");  jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"id\":", "\"Galaxy ID\":");
-      jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyNumStars\":", "\"Number of Stars\":"); jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyDist\":", "\"Galaxy Distance From Earth\":");
-      jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyWidth\":", "\"Diameter\":"); jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyBrightness\":", "\"Galaxy Brightness (Apparent Magnitude)\":");
-      
-      let rand_galaxy_col1 = JSON.parse(jsonGalaxyNameCol1);
-    
-      let myJSONcol2 = JSON.stringify(rand_galaxy, ["galaxyDescr"]);      
-      let rand_galaxy_col2 = JSON.parse(myJSONcol2);
-      
-      let myJSONcol3 = JSON.stringify(rand_galaxy, ["galaxyImgUrl"]);      
-      let rand_galaxy_col3 = JSON.parse(myJSONcol3);
-      
-      const generatedHtmlCol1 = Object.keys(rand_galaxy_col1).reduce((accum, currKey) => accum +
-      `<div class="p-2">
-        <h4 class="o-stars">${currKey}: ${rand_galaxy_col1[currKey]}</h4>
-        </div>`, '');
-    
-              document.getElementById('container-planet-se-a').innerHTML = generatedHtmlCol1;
-    
-      const generatedHtmlCol2 = Object.keys(rand_galaxy_col2).reduce((accum, currKey) => accum +
-        `<div class="p-2">
-          <h6 class="o-stars">${rand_galaxy_col2[currKey]}</h6>
-          </div>`, '');
-    
-      const generatedHtmlCol3 = Object.keys(rand_galaxy_col3).reduce((accum, currKey) => accum +
-          `<div class="p-2">
-            <img src ="${rand_galaxy_col3[currKey]}" style = "border: 5px solid #555;"width= "300px" height= "300px"</img>
-            </div>`, '');
-    
-                document.getElementById('container-planet-se-b').innerHTML = generatedHtmlCol3 + generatedHtmlCol2;
-    
-    }
+async function getPlanetDataSE() {
+  const response = await fetch(planet_url);
+  const data = await response.json();
+  const rand_planet = data[getRandomInt(4,8)]
+  /*Now I know what you're thinking. What on earth is this mess below this comment.
+    How about we just be happy that it works :)*/
 
-async function getGalaxyDataPeculiar() {
-    const response = await fetch(galaxy_url);
-    const data = await response.json();
-    const rand_galaxy = data[getRandomInt(8,12)]
-    /*Now I know what you're thinking. What on earth is this mess below this comment.
-      How about we just be happy that it works :)*/
-  
-      let myJSONcol1 = JSON.stringify(rand_galaxy, ["galaxyName", "id", "galaxyNumStars", "galaxyDist", "galaxyWidth", "galaxyBrightness"]);
-      jsonGalaxyNameCol1 = myJSONcol1.replace("\"galaxyName\":", "\"Galaxy Name\":");  jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"id\":", "\"Galaxy ID\":");
-      jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyNumStars\":", "\"Number of Stars\":"); jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyDist\":", "\"Galaxy Distance From Earth\":");
-      jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyWidth\":", "\"Diameter\":"); jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyBrightness\":", "\"Galaxy Brightness (Apparent Magnitude)\":");
-      
-      let rand_galaxy_col1 = JSON.parse(jsonGalaxyNameCol1);
-    
-      let myJSONcol2 = JSON.stringify(rand_galaxy, ["galaxyDescr"]);      
-      let rand_galaxy_col2 = JSON.parse(myJSONcol2);
-      
-      let myJSONcol3 = JSON.stringify(rand_galaxy, ["galaxyImgUrl"]);      
-      let rand_galaxy_col3 = JSON.parse(myJSONcol3);
-      
-      const generatedHtmlCol1 = Object.keys(rand_galaxy_col1).reduce((accum, currKey) => accum +
-      `<div class="p-2">
-        <h4 class="o-stars">${currKey}: ${rand_galaxy_col1[currKey]}</h4>
-        </div>`, '');
-    
-              document.getElementById('container-peculiara').innerHTML = generatedHtmlCol1;
-    
-      const generatedHtmlCol2 = Object.keys(rand_galaxy_col2).reduce((accum, currKey) => accum +
-        `<div class="p-2">
-          <h6 class="o-stars">${rand_galaxy_col2[currKey]}</h6>
-          </div>`, '');
-    
-      const generatedHtmlCol3 = Object.keys(rand_galaxy_col3).reduce((accum, currKey) => accum +
-          `<div class="p-2">
-            <img src ="${rand_galaxy_col3[currKey]}" style = "border: 5px solid #555;"width= "300px" height= "300px"</img>
-            </div>`, '');
-    
-                document.getElementById('container-peculiarb').innerHTML = generatedHtmlCol3 + generatedHtmlCol2;
-    
-    }
+    let myJSONcol1 = JSON.stringify(rand_planet, ["planetName", "id", "planetType", "planetMass", "planetRadius", "planetTilt", "planetDistFromEarth"]);
+    jsonPlanetNameCol1 = myJSONcol1.replace("\"planetName\":", "\"Planet Name\":");  jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"id\":", "\"Planet ID\":");
+    jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetType\":", "\"Type of Planet\":"); jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetMass\":", "\"Mass\":");
+    jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetRadius\":", "\"Radius\":"); jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetTilt\":", "\"Tilt\":");
+    jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetDistFromEarth\":", "\"Planet Distance From Earth\":");
 
-async function getGalaxyDataIrregular() {
-    const response = await fetch(galaxy_url);
-    const data = await response.json();
-    const rand_galaxy = data[getRandomInt(12,15)]
-    /*Now I know what you're thinking. What on earth is this mess below this comment.
-      How about we just be happy that it works :)*/
+    let rand_planet_col1 = JSON.parse(jsonPlanetNameCol1);
   
-    let myJSONcol1 = JSON.stringify(rand_galaxy, ["galaxyName", "id", "galaxyNumStars", "galaxyDist", "galaxyWidth", "galaxyBrightness"]);
-    jsonGalaxyNameCol1 = myJSONcol1.replace("\"galaxyName\":", "\"Galaxy Name\":");  jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"id\":", "\"Galaxy ID\":");
-    jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyNumStars\":", "\"Number of Stars\":"); jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyDist\":", "\"Galaxy Distance From Earth\":");
-    jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyWidth\":", "\"Diameter\":"); jsonGalaxyNameCol1 =  jsonGalaxyNameCol1.replace("\"galaxyBrightness\":", "\"Galaxy Brightness (Apparent Magnitude)\":");
+    let myJSONcol2 = JSON.stringify(rand_planet, ["planetDescr"]);      
+    let rand_planet_col2 = JSON.parse(myJSONcol2);
     
-    let rand_galaxy_col1 = JSON.parse(jsonGalaxyNameCol1);
-  
-    let myJSONcol2 = JSON.stringify(rand_galaxy, ["galaxyDescr"]);      
-    let rand_galaxy_col2 = JSON.parse(myJSONcol2);
+    let myJSONcol3 = JSON.stringify(rand_planet, ["planetImgUrl"]);      
+    let rand_planet_col3 = JSON.parse(myJSONcol3);
     
-    let myJSONcol3 = JSON.stringify(rand_galaxy, ["galaxyImgUrl"]);      
-    let rand_galaxy_col3 = JSON.parse(myJSONcol3);
-    
-    const generatedHtmlCol1 = Object.keys(rand_galaxy_col1).reduce((accum, currKey) => accum +
+    const generatedHtmlCol1 = Object.keys(rand_planet_col1).reduce((accum, currKey) => accum +
     `<div class="p-2">
-      <h4 class="o-stars">${currKey}: ${rand_galaxy_col1[currKey]}</h4>
+      <h4 class="o-stars">${currKey}: ${rand_planet_col1[currKey]}</h4>
       </div>`, '');
   
-            document.getElementById('container-irregulara').innerHTML = generatedHtmlCol1;
+            document.getElementById('container-planet-se-a').innerHTML = generatedHtmlCol1;
   
-    const generatedHtmlCol2 = Object.keys(rand_galaxy_col2).reduce((accum, currKey) => accum +
+    const generatedHtmlCol2 = Object.keys(rand_planet_col2).reduce((accum, currKey) => accum +
       `<div class="p-2">
-        <h6 class="o-stars">${rand_galaxy_col2[currKey]}</h6>
+        <h6 class="o-stars">${rand_planet_col2[currKey]}</h6>
         </div>`, '');
   
-    const generatedHtmlCol3 = Object.keys(rand_galaxy_col3).reduce((accum, currKey) => accum +
+    const generatedHtmlCol3 = Object.keys(rand_planet_col3).reduce((accum, currKey) => accum +
         `<div class="p-2">
-          <img src ="${rand_galaxy_col3[currKey]}" style = "border: 5px solid #555;"width= "300px" height= "300px"</img>
+          <img src ="${rand_planet_col3[currKey]}" style = "border: 5px solid #555;"width= "300px" height= "300px"</img>
           </div>`, '');
   
-              document.getElementById('container-irregularb').innerHTML = generatedHtmlCol3 + generatedHtmlCol2;
+              document.getElementById('container-planet-se-b').innerHTML = generatedHtmlCol3 + generatedHtmlCol2;
+  
+  }
+
+async function getPlanetDataGG() {
+  const response = await fetch(planet_url);
+  const data = await response.json();
+  const rand_planet = data[getRandomInt(8,12)]
+  /*Now I know what you're thinking. What on earth is this mess below this comment.
+    How about we just be happy that it works :)*/
+
+    let myJSONcol1 = JSON.stringify(rand_planet, ["planetName", "id", "planetType", "planetMass", "planetRadius", "planetTilt", "planetDistFromEarth"]);
+    jsonPlanetNameCol1 = myJSONcol1.replace("\"planetName\":", "\"Planet Name\":");  jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"id\":", "\"Planet ID\":");
+    jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetType\":", "\"Type of Planet\":"); jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetMass\":", "\"Mass\":");
+    jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetRadius\":", "\"Radius\":"); jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetTilt\":", "\"Tilt\":");
+    jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetDistFromEarth\":", "\"Planet Distance From Earth\":");
+
+    let rand_planet_col1 = JSON.parse(jsonPlanetNameCol1);
+  
+    let myJSONcol2 = JSON.stringify(rand_planet, ["planetDescr"]);      
+    let rand_planet_col2 = JSON.parse(myJSONcol2);
+    
+    let myJSONcol3 = JSON.stringify(rand_planet, ["planetImgUrl"]);      
+    let rand_planet_col3 = JSON.parse(myJSONcol3);
+    
+    const generatedHtmlCol1 = Object.keys(rand_planet_col1).reduce((accum, currKey) => accum +
+    `<div class="p-2">
+      <h4 class="o-stars">${currKey}: ${rand_planet_col1[currKey]}</h4>
+      </div>`, '');
+  
+            document.getElementById('container-planet-gg-a').innerHTML = generatedHtmlCol1;
+  
+    const generatedHtmlCol2 = Object.keys(rand_planet_col2).reduce((accum, currKey) => accum +
+      `<div class="p-2">
+        <h6 class="o-stars">${rand_planet_col2[currKey]}</h6>
+        </div>`, '');
+  
+    const generatedHtmlCol3 = Object.keys(rand_planet_col3).reduce((accum, currKey) => accum +
+        `<div class="p-2">
+          <img src ="${rand_planet_col3[currKey]}" style = "border: 5px solid #555;"width= "300px" height= "300px"</img>
+          </div>`, '');
+  
+              document.getElementById('container-planet-gg-b').innerHTML = generatedHtmlCol3 + generatedHtmlCol2;
+  
+  }
+
+async function getPlanetDataNL() {
+  const response = await fetch(planet_url);
+  const data = await response.json();
+  const rand_planet = data[getRandomInt(12,16)]
+  /*Now I know what you're thinking. What on earth is this mess below this comment.
+    How about we just be happy that it works :)*/
+
+    let myJSONcol1 = JSON.stringify(rand_planet, ["planetName", "id", "planetType", "planetMass", "planetRadius", "planetTilt", "planetDistFromEarth"]);
+    jsonPlanetNameCol1 = myJSONcol1.replace("\"planetName\":", "\"Planet Name\":");  jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"id\":", "\"Planet ID\":");
+    jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetType\":", "\"Type of Planet\":"); jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetMass\":", "\"Mass\":");
+    jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetRadius\":", "\"Radius\":"); jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetTilt\":", "\"Tilt\":");
+    jsonPlanetNameCol1 =  jsonPlanetNameCol1.replace("\"planetDistFromEarth\":", "\"Planet Distance From Earth\":");
+
+    let rand_planet_col1 = JSON.parse(jsonPlanetNameCol1);
+  
+    let myJSONcol2 = JSON.stringify(rand_planet, ["planetDescr"]);      
+    let rand_planet_col2 = JSON.parse(myJSONcol2);
+    
+    let myJSONcol3 = JSON.stringify(rand_planet, ["planetImgUrl"]);      
+    let rand_planet_col3 = JSON.parse(myJSONcol3);
+    
+    const generatedHtmlCol1 = Object.keys(rand_planet_col1).reduce((accum, currKey) => accum +
+    `<div class="p-2">
+      <h4 class="o-stars">${currKey}: ${rand_planet_col1[currKey]}</h4>
+      </div>`, '');
+  
+            document.getElementById('container-planet-nl-a').innerHTML = generatedHtmlCol1;
+  
+    const generatedHtmlCol2 = Object.keys(rand_planet_col2).reduce((accum, currKey) => accum +
+      `<div class="p-2">
+        <h6 class="o-stars">${rand_planet_col2[currKey]}</h6>
+        </div>`, '');
+  
+    const generatedHtmlCol3 = Object.keys(rand_planet_col3).reduce((accum, currKey) => accum +
+        `<div class="p-2">
+          <img src ="${rand_planet_col3[currKey]}" style = "border: 5px solid #555;"width= "300px" height= "300px"</img>
+          </div>`, '');
+  
+              document.getElementById('container-planet-nl-b').innerHTML = generatedHtmlCol3 + generatedHtmlCol2;
   
   }
 
