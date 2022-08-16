69c8371fc92f0ce7350b7c4eddcc3575fbff2aad
Danny Maksoud <dannymaksoud@Dannys-MacBook-Air.local>
2022-08-16 10:37:57 +0100

    Delete method now working, all I need to do is refactor the same methods for galaxies and planets and then make sure the error messages match what is actually happening

--- AstroAPIBackend/src/main/resources/static/staruserinput.js
+++ AstroAPIBackend/src/main/resources/static/staruserinput.js
@@ -91,3 +91,26 @@ formstarupdate.addEventListener('submit', function(e){
      body.innerHTML = data.body  
    }).catch(error => console.error('Error:', error)); 
    });
+
+   formstardelete.addEventListener('submit', function(e){
+    e.preventDefault()
+   
+    let stariddelete = document.getElementById('stariddelete').value;
+   
+    fetch(`http://localhost:8080/delete/${stariddelete}`, {
+     method: 'DELETE'
+     ,
+     headers: {
+       'Content-type': 'application/json; charset=UTF-8',
+     }
+     })
+     .then(function(response){ 
+     return response.json()})
+     .then(function(data)
+     {console.log(data)
+     title=document.getElementById("title")
+     body=document.getElementById("bd")
+     title.innerHTML = data.title
+     body.innerHTML = data.body  
+   }).catch(error => console.error('Error:', error)); 
+   });
